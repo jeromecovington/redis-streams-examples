b87caa220e1d939af91ea798c49854703d36c7c1
Jerome Covington <jeromecovington@gmail.com>
2019-08-10 11:30:27 -0400

    add group to utils, destroy group in reset

--- reset.js
+++ reset.js
@@ -1,13 +1,16 @@
-const { client, key } = require('./utils')
+const { client, key, group } = require('./utils')
 const date = new Date()
 
 function main () {
   async function del () {
     const c = client()
 
-    await c.delAsync(key)
+    await c.xgroupAsync('DESTROY', key, group)
+    console.log(`${date.getTime()}: Streams group "${group}" destroyed.`)
 
+    await c.delAsync(key)
     console.log(`${date.getTime()}: Streams key "${key}" deleted.`)
+
     process.exit()
   }
 
