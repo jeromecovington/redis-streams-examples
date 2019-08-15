9ec272ee8cc216b9d9542c18fb815cb00659d438
Jerome Covington <jeromecovington@gmail.com>
2019-08-15 16:32:57 -0400

    documents commands impl (#2)

--- producer-a.js
+++ producer-a.js
@@ -7,12 +7,26 @@ function main () {
   async function produce () {
     const c = client()
 
-    // Log informational stream growth statistics.
+    /**
+     * https://redis.io/commands/xlen
+     * Returns the number of entries inside a stream.
+     **/
     const length = await c.xlenAsync(key)
+
+    // Log informational stream growth statistics.
     console.log(`${date.getTime()}: Stream ${key} has ${length} messages.`)
 
-    // Append the next message.
-    // https://redis.io/commands/xadd
+    /**
+     * https://redis.io/commands/xadd
+     * Appends the next message.
+     *
+     * Arguments:
+     * key - The name of our stream.
+     * '*' - Autogenerate id for the stream.
+     * 'n' - The name of the field entry.
+     * n - The value for the field entry.
+     * (Any number of field entry name/value pairs may be added.
+     **/
     const id = await c.xaddAsync(key, '*', 'n', n)
     console.log(`${date.getTime()}: Produced the number ${n} as message id ${id}`)
 
--- producer-b.js
+++ producer-b.js
@@ -9,15 +9,29 @@ function main () {
     const c = client()
 
     if (n > max) {
-      // log informational stream growth statistics.
+      /**
+       * https://redis.io/commands/xlen
+       * Returns the number of entries inside a stream.
+       **/
       const length = await c.xlenAsync(key)
+
+      // log informational stream growth statistics.
       console.log(`${date.getTime()}: Stream "${key}" has ${length} messages.`)
 
       process.exit()
     }
 
-    // Append the next message.
-    // https://redis.io/commands/xadd
+    /**
+     * https://redis.io/commands/xadd
+     * Appends the next message.
+     *
+     * Arguments:
+     * key - The name of our stream.
+     * '*' - Autogenerate id for the stream.
+     * 'n' - The name of the field entry.
+     * n - The value for the field entry.
+     * (Any number of field entry name/value pairs may be added.
+     **/
     const id = await c.xaddAsync(key, '*', 'n', n)
     console.log(`${date.getTime()}: Produced the number ${n} as message id ${id}`)
 
